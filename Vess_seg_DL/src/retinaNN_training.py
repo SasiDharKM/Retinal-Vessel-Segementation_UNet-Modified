266c771ceb7bac32affae315cc8ada433c85e881
sasidharkm <sasidhar.km97@gmail.com>
2018-06-21 12:59:17 +0530

    Code for viewing how the model looks like

--- Vess_seg_DL/src/retinaNN_training.py
+++ Vess_seg_DL/src/retinaNN_training.py
@@ -79,3 +79,18 @@ patches_imgs_train, patches_masks_train = get_data_training(
     inside_FOV = config.getboolean('training settings', 'inside_FOV') #select the patches only inside the FOV  (default == True)
 )
 
+N_sample = min(patches_imgs_train.shape[0],40)
+visualize(group_images(patches_imgs_train[0:N_sample,:,:,:],5),'./'+name_experiment+'/'+"sample_input_imgs")#.show()
+visualize(group_images(patches_masks_train[0:N_sample,:,:,:],5),'./'+name_experiment+'/'+"sample_input_masks")#.show()
+
+# Construct the model architecture and save it
+
+n_ch = patches_imgs_train.shape[1]
+patch_height = patches_imgs_train.shape[2]
+patch_width = patches_imgs_train.shape[3]
+model = get_unet(n_ch, patch_height, patch_width)
+print "Check: final output of the network:"
+print model.output_shape
+plot(model, to_file='./'+name_experiment+'/'+name_experiment + '_model.png')
+json_string = model.to_json()
+open('./'+name_experiment+'/'+name_experiment +'_architecture.json', 'w').write(json_string)
