07922b168100bce43c55086ce37be48aa7308b0b
sasidharkm <sasidhar.km97@gmail.com>
2018-06-22 12:05:23 +0530

    defined some flags and directories

--- Vess_seg_DL/train.py
+++ Vess_seg_DL/train.py
@@ -7,3 +7,17 @@ config.readfp(open(r'./configuration.txt'))
 
 name_experiment = config.get('experiment name', 'name')
 nohup = config.getboolean('training settings', 'nohup')
+
+run_GPU = '' if sys.platform == 'win32' else ' THEANO_FLAGS=device=gpu,floatX=float32 '
+
+# making a directory if not present 
+
+result_dir = name_experiment
+print "\n1. Create directory for the results (if not already existing)"
+if os.path.exists(result_dir):
+    print "Dir already existing"
+elif sys.platform=='win32':
+    os.system('mkdir ' + result_dir)
+else:
+    os.system('mkdir -p ' +result_dir)
+
