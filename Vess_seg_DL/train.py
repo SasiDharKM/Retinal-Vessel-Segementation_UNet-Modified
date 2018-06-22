f1685cf5cd729ef7bb17135706e44d069ee0bca8
sasidharkm <sasidhar.km97@gmail.com>
2018-06-22 12:08:52 +0530

    wrote some lines to save the experiment paramaters and results

--- Vess_seg_DL/train.py
+++ Vess_seg_DL/train.py
@@ -21,3 +21,10 @@ elif sys.platform=='win32':
 else:
     os.system('mkdir -p ' +result_dir)
 
+
+print "copy the configuration file in the results folder"
+if sys.platform=='win32':
+    os.system('copy configuration.txt .\\' +name_experiment+'\\'+name_experiment+'_configuration.txt')
+else:
+    os.system('cp configuration.txt ./' +name_experiment+'/'+name_experiment+'_configuration.txt')
+
