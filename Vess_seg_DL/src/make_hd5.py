e127b1a35992ac037d06c1670e8978cf58fc8053
sasidharkm <sasidhar.km97@gmail.com>
2018-06-22 11:36:31 +0530

    Added the paths to the org images

--- Vess_seg_DL/src/make_hd5.py
+++ Vess_seg_DL/src/make_hd5.py
@@ -3,3 +3,14 @@ import h5py
 import numpy as np
 from PIL import Image
 
+def write_hdf5(arr,outfile):
+  with h5py.File(outfile,"w") as f:
+    f.create_dataset("image", data=arr, dtype=arr.dtype)
+
+original_imgs_train = "./DRIVE/training/images/"
+groundTruth_imgs_train = "./DRIVE/training/1st_manual/"
+borderMasks_imgs_train = "./DRIVE/training/mask/"
+
+original_imgs_test = "./DRIVE/test/images/"
+groundTruth_imgs_test = "./DRIVE/test/1st_manual/"
+borderMasks_imgs_test = "./DRIVE/test/mask/"
